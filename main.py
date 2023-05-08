11859d08a318734c1cbd21c835c7df4f0b4f402f
Killereq-PL <86300923+Killereq-PL@users.noreply.github.com>
2023-05-08 15:56:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+class susObject:
+    def __init__(self):
+        print("susCMD initialized")
+        print("input 'help' to see all commands \n")
+    def echo(self, arg1: str) -> None:
+        print(arg1)
+    def help(self):
+        print("""commands: {
+help (shows all commands),
+echo <string>,
+exit,
+program <filename> (enters program mode)
+}""")
+    def program(self, filename: str) -> None:
+        print("program mode enabled ("+filename+")")
+        fn = filename+".sus"
+        try:
+            file = open(fn, 'r')
+        except IOError:
+            file = open(fn, 'w')
+            file.mode = "r"
+        if file.read() != "":
+            print("program contents:")
+            print(file.read())
+            print("\n")
+            lines = []
+        else:
+            lines = file.read().splitlines()
+        file.mode = "r+"
+        print("choose mode {modify <line> <modified>, run, overwrite, cancel}")
+        cmds = ["finish", "echo", "delay"]
+        mode = input("> ")
+        if "overwrite" in mode:
+            lines = []
+            i = 0
+            while True:
+                i += 1
+                cl = input(str(i)+" ")
+                if cl in cmds: #TODO: FIX
+                    lines.append(cl)
+                else:
+                    print("command does not exist")
+                    i -= 1
+                    continue
+
+sus = susObject()
+while True:
+    a = str(input("> "))
+    if "echo" in a:
+        b = a.replace("echo ", "")
+        sus.echo(b)
+    if "exit" in a:
+        break
+    if "help" in a:
+        sus.help()
+    if "program" in a:
+        b = a.replace("program ", "")
+        sus.program(b)
